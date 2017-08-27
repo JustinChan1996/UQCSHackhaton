355332be307dd569d1c5887fb4cf02fd3c2e25c8
JUSTIN_CHAN\Lenovo <justin15071996@gmail.com>
2017-08-27 15:57:27 +1000

    Added the code to measure the laps

--- UQCS_Hackhaton_PhoneDeployment/app/src/main/java/com/example/lenovo/uqcs_hackhaton_phonedeployment/MainActivity.java
+++ UQCS_Hackhaton_PhoneDeployment/app/src/main/java/com/example/lenovo/uqcs_hackhaton_phonedeployment/MainActivity.java
@@ -21,9 +21,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private int counter = 0;
     // device sensor manager
     private SensorManager mSensorManager;
-    // record the initial bearing of the smartphone when the lap starts
-    private float initialLapDegrees = 0;
-
+    // define accelerometer sensor
+    private Sensor accelerometer;
+    // define magtenometer sensor
+    private Sensor magnetometer;
+    // boolean flag to indicate if application has just been used. If it is just started. The value is false. Else, it is true
+    private boolean flag = false;
+    // record the initial bearing of the smartphone when the lap starts. Put the initial value as an nonsensical value since (-180 degrees <angle<180 degrees)
+    private int initialLapDegrees;
+    float[] mGravity;
+    float[] mGeomagnetic;
+    float angle;
+    private TextView azimut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,11 +42,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             lapCounter = (TextView)findViewById(R.id.laps);
             minus_button = (Button) findViewById(R.id.button_minus);
             reset_button = (Button) findViewById(R.id.button_reset);
-            // initialize your android device sensor capabilities
-            mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+            azimut = (TextView) findViewById(R.id.azimut);
+            mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+            accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
-
-            lapCounter.setOnClickListener(new View.OnClickListener()
+        lapCounter.setOnClickListener(new View.OnClickListener()
             {
                 @Override
                 public void onClick(View v)
@@ -101,7 +111,51 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent sensorEvent)
     {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        {
+            mGravity = sensorEvent.values;
+        }
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+        {
+            mGeomagnetic = sensorEvent.values;
+        }
+        if (mGravity != null && mGeomagnetic != null)
+        {
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success)
+            {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                angle = orientation[0]; // orientation contains: azimut, pitch and roll
 
+            }
+        }
+        float degrees = -angle * 360/(2*3.14159f);
+        int degreesInteger = (int) degrees;
+        azimut.setText(Integer.toString(degreesInteger));
+        // if the app is launched(initial degree values should be -181)
+        if(flag == false)
+        {
+            // set the initial bearings to the sensor values
+            initialLapDegrees = degreesInteger;
+            flag = true;
+        }
+        // check if the difference between the sensor readout and initial angle is greater than 110 degrees. If yes, replace the initialLapDegrees with the present readout and increment the  lap counter by 1
+        if((Math.abs(initialLapDegrees-degreesInteger))>= 110)
+        {
+            initialLapDegrees = degreesInteger;
+            counter = counter + 1;
+            if(counter < 10)
+            {
+                lapCounter.setText("0"+ counter);
+            }
+            else
+            {
+                lapCounter.setText(Integer.toString(counter));
+            }
+        }
     }
 
     @Override
@@ -111,12 +165,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
-
-        // for the system's orientation sensor registered listeners
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-                SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
     }
 
     @Override
